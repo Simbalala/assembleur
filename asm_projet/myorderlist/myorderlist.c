3cd8c7d4f8a86f2b0a36e7845e96defe79021f88
Mathieu Tourrette <mathieu.tourrette@epita.fr>
2022-02-04 01:56:09 +0100

    my order, print int reverse

--- asm_projet/myorderlist/myorderlist.c
+++ asm_projet/myorderlist/myorderlist.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int  orderlist(long long *list, int size);
+int orderlist(long long *list, int size);
 
 int write_list(char *s);
 
@@ -8,11 +8,7 @@ long long li[] = {0x200, 0x3, 0x8};
 
 int main() {
     int value = orderlist(li, 3);
-    for (int i= 0; i < 3; i++)
-    {
-        printf("%d, ",li[i]);
-    }
-    printf("\n%d\n", value);
+    printf("return %d\n", value);
     // printf("%s\n", orderlist(li, 3));
     // print: "3, 8, 16\n"
 
