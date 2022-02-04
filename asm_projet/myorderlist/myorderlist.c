5bfd131a6ab2533db55d403f4df7aeb441de5b22
Mathieu Tourrette <mathieu.tourrette@epita.fr>
2022-02-04 16:53:50 -0500

    bug print number

--- asm_projet/myorderlist/myorderlist.c
+++ asm_projet/myorderlist/myorderlist.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
-char* orderlist(long long *list, int size);
+int orderlist(long long *list, int size);
 
 int write_list(char *s);
 
 long long li[] = {0x200, 0x3, 0x8};
 
 int main() {
-    char* value = orderlist(li, 3);
-    // printf("return %s\n", value);
+    int value = orderlist(li, 3);
+    printf("return %d\n", value);
     // printf("%s\n", orderlist(li, 3));
     // print: "3, 8, 16\n"
 
