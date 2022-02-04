49708199e11c5c585e859d4aeb4e3f7cbe10a1b8
Mathieu Tourrette <mathieu.tourrette@epita.fr>
2022-02-04 17:45:47 +0100

    correction list order, print number with buffer and v1 comma, eof

--- asm_projet/myorderlist/myorderlist.c
+++ asm_projet/myorderlist/myorderlist.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
-int orderlist(long long *list, int size);
+char* orderlist(long long *list, int size);
 
 int write_list(char *s);
 
 long long li[] = {0x200, 0x3, 0x8};
 
 int main() {
-    int value = orderlist(li, 3);
-    printf("return %d\n", value);
+    char* value = orderlist(li, 3);
+    // printf("return %s\n", value);
     // printf("%s\n", orderlist(li, 3));
     // print: "3, 8, 16\n"
 
