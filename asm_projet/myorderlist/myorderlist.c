b3653cf73abfcfdae81f43452de616481063d6a3
Mathieu Tourrette <mathieu.tourrette@epita.fr>
2022-02-07 18:05:00 -0500

    correction print

--- asm_projet/myorderlist/myorderlist.c
+++ asm_projet/myorderlist/myorderlist.c
@@ -1,16 +1,11 @@
 #include <stdio.h>
 
-int orderlist(long long *list, int size);
+void orderlist(long long *list, int size);
 
-int write_list(char *s);
-
-long long li[] = {0x200, 0x3, 0x8};
+long long li[] = {0x200, 0x3, 0x8, 0x7};
 
 int main() {
-    int value = orderlist(li, 3);
-    printf("return %d\n", value);
-    // printf("%s\n", orderlist(li, 3));
-    // print: "3, 8, 16\n"
+    orderlist(li, 4);
 
     return 0;
 }
